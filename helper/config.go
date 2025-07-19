5b06607b9c35503b430e9da92f9e8be5fb7b2045
w1zardOZ <67543663+w1zardOZ@users.noreply.github.com>
2025-07-19 15:47:30 +0530

    env file added

--- helper/config.go
+++ helper/config.go
@@ -1,11 +1,22 @@
 package helper
 
+import (
+	"log"
+	"github.com/joho/godotenv"
+	"os"
+)
+
 type Config struct {
 	// Path to the config file
 	MONGO_DB_CONNECTION_STRING string
 }
 
 func NewConfig() *Config {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found or error loading .env file")
+	}
 	return &Config{
 		MONGO_DB_CONNECTION_STRING: "mongodb://localhost:27017",
 	}
